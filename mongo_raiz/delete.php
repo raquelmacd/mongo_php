0d680a7e81bcd5954bc12288d7df024591355412
raquelmacd <raquel.macedo.1195@gmail.com>
2020-05-10 16:09:21 -0300

    Posto com git bash - v1.0

---
+++ mongo_raiz/delete.php
@@ -0,0 +1,10 @@
+<?php
+    
+$manager = new \MongoDB\Driver\Manager("mongodb://localhost:27017");
+$bw = new \MongoDB\Driver\BulkWrite;
+
+$filter=['nome'=>'Maria'];
+//$option[]
+$bw->delete($filter);
+
+$manager->executeBulkWrite('raiz.academicos',$bw);
